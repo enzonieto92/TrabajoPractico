3bcde06b0f50f46d5146e2781060a03447af8fc7
MNRocha <mnrrocha@hotmail.com>
2023-05-01 19:46:39 -0300

    textBox en blanco

--- TP5_Grupo_Nro_06/AgregarSucursal.aspx.designer.cs
+++ TP5_Grupo_Nro_06/AgregarSucursal.aspx.designer.cs
@@ -213,15 +213,6 @@ namespace TP5_Grupo_Nro_06
         protected global::System.Web.UI.WebControls.Label lblAgregado;
 
         /// <summary>
-        /// Control lblPrueba.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblPrueba;
-
-        /// <summary>
         /// Control ValidationSummary1.
         /// </summary>
         /// <remarks>
