0c64aeb3e9fc5b39adfad539c7900f881f5823ac
Enzo Matías Nieto <enzonieto92@hotmail.com>
2023-05-04 02:07:25 -0300

    Backing Black

--- TP5_Grupo_Nro_06/ListarSucursal.aspx.cs
+++ TP5_Grupo_Nro_06/ListarSucursal.aspx.cs
@@ -27,14 +27,6 @@ namespace TP5_Grupo_Nro_06
         {
             grdSucursales.DataSource = listaSucursal.listarSucursal(id);
             grdSucursales.DataBind();
-            if (grdSucursales.Rows.Count > 0)
-            {
-                lblResultado.Visible = false;
-            }
-            else
-            {
-                lblResultado.Visible = true;
-            }
         }
         protected void btnAgregarSucursal_Click(object sender, EventArgs e)
         {
