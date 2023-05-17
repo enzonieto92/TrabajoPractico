443c6a76065bd7f5290a29a36289c80968b59e1a
Enzo Matías Nieto <enzonieto92@hotmail.com>
2023-05-16 22:53:45 -0300

    Visual MostrarProductos.aspx

--- principalForm/principalForm/MostrarProductos.aspx
+++ principalForm/principalForm/MostrarProductos.aspx
@@ -6,10 +6,28 @@
 <head runat="server">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title></title>
+    <style>
+        /*.form{
+            text-align: center;
+        }
+        .grid{
+            justify-content: center;
+        }*/
+    </style>
 </head>
 <body>
     <form id="form1" runat="server">
-        <div>
+        <div class="form";>
+        <asp:Label ID="lblSeleccionados" runat="server" Text="Productos Seleccionados por el Usuario: " Font-Bold="True" Font-Size="X-Large"></asp:Label>
+        </div>
+
+        <div class="grid";>
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+        </div>
+
+        <div class="form";>
+        <asp:HyperLink ID="hpVolver" runat="server" NavigateUrl="~/InicioEj2.aspx">Volver al Inicio</asp:HyperLink>
         </div>
     </form>
 </body>
--- principalForm/principalForm/MostrarProductos.aspx.designer.cs
+++ principalForm/principalForm/MostrarProductos.aspx.designer.cs
@@ -22,5 +22,32 @@ namespace principalForm
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control lblSeleccionados.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblSeleccionados;
+
+        /// <summary>
+        /// Control GridView1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
+
+        /// <summary>
+        /// Control hpVolver.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hpVolver;
     }
 }
