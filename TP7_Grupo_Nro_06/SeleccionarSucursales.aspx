d27314b73882c96088e65e9cdcb3ad6ee79703c6
Enzo Matías Nieto <enzonieto92@hotmail.com>
2023-05-19 22:29:21 -0300

    TP 7 primer aporte

---
+++ TP7_Grupo_Nro_06/SeleccionarSucursales.aspx
@@ -0,0 +1,98 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SeleccionarSucursales.aspx.cs" Inherits="TP7_Grupo_Nro_06.SeleccionarSucursales" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style>
+        .container {
+        margin-bottom: 30px;
+        overflow: hidden; /* Limpiar el float */
+        }
+        td {
+          width: 100%; /* Ancho del 100% de la celda de la tabla */
+        }
+        .Boton{
+            width:120px;
+            margin-bottom: 30px;
+        }
+        .left-column {
+            float: left;
+            width:130px;
+        }
+
+        .right-column {
+                float: left;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class ="container">
+            <asp:HyperLink ID="linkListado" runat="server">Listado de Sucursales</asp:HyperLink>
+            <asp:HyperLink ID="linkMostrarSeleccionados" runat="server">Mostrar Sucursales Seleccionadas</asp:HyperLink>
+            </div>
+            <div>
+            <asp:Label ID="lblListado" runat="server" Text="Listado de Sucursales" Font-Bold="True" Font-Size="X-Large"></asp:Label>
+        </div>
+            <div class ="container">
+        <asp:Label ID="lblBusqueda" runat="server" Text="Búsqueda por Nombre de Sucursal"></asp:Label>
+        <asp:TextBox ID="txtboxBusqueda" runat="server"></asp:TextBox>
+        <asp:Button ID="btnBuscar" runat="server" Text="Buscar" />
+            </div>
+        <div class="container">
+              <table class="left-column">
+      <tr>
+        <td>
+            <asp:Button ID="btnBuenosAires" runat="server" Text="Buenos Aires" CssClass="Boton" />
+          
+        </td>
+      </tr>
+      <tr>
+        <td>
+            <asp:Button ID="btnEntreRios" runat="server" Text="Entre Rios" CssClass="Boton" />
+
+        </td>
+      </tr>
+      <tr>
+        <td>
+            <asp:Button ID="btnSantaFe" runat="server" Text="Santa Fé" CssClass="Boton" />
+          
+        </td>
+      </tr>
+          <tr>
+        <td>
+            <asp:Button ID="btnLaPampa" runat="server" Text="La Pampa" CssClass="Boton" />
+          
+        </td>
+      </tr>
+          <tr>
+        <td>
+            <asp:Button ID="btnCórdoba" runat="server" Text="Córdoba" CssClass="Boton"/>
+          
+        </td>
+      </tr>
+          <tr>
+        <td>
+            <asp:Button ID="btnMisiones" runat="server" Text="Misiones" CssClass="Boton" />
+          
+        </td>
+      </tr>
+          <tr>
+        <td>
+            <asp:Button ID="btnChaco" runat="server" Text="Chaco" CssClass="Boton" />
+          
+        </td>
+      </tr>
+    </table>
+
+      <div class="right-column">
+                <asp:ListView ID="lvSucursales" runat="server">
+                </asp:ListView>
+        </div>
+</div>
+    </form>
+</body>
+</html>
---
+++ TP7_Grupo_Nro_06/SeleccionarSucursales.aspx.designer.cs
@@ -0,0 +1,152 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace TP7_Grupo_Nro_06
+{
+
+
+    public partial class SeleccionarSucursales
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control linkListado.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink linkListado;
+
+        /// <summary>
+        /// Control linkMostrarSeleccionados.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink linkMostrarSeleccionados;
+
+        /// <summary>
+        /// Control lblListado.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblListado;
+
+        /// <summary>
+        /// Control lblBusqueda.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblBusqueda;
+
+        /// <summary>
+        /// Control txtboxBusqueda.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtboxBusqueda;
+
+        /// <summary>
+        /// Control btnBuscar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnBuscar;
+
+        /// <summary>
+        /// Control btnBuenosAires.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnBuenosAires;
+
+        /// <summary>
+        /// Control btnEntreRios.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnEntreRios;
+
+        /// <summary>
+        /// Control btnSantaFe.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSantaFe;
+
+        /// <summary>
+        /// Control btnLaPampa.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnLaPampa;
+
+        /// <summary>
+        /// Control btnCórdoba.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnCórdoba;
+
+        /// <summary>
+        /// Control btnMisiones.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnMisiones;
+
+        /// <summary>
+        /// Control btnChaco.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnChaco;
+
+        /// <summary>
+        /// Control lvSucursales.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListView lvSucursales;
+    }
+}
