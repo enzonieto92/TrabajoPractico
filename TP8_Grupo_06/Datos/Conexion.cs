356c5149caf36f0d0946aecd25db0a79c24e09fe
axel mazo <axel.98.mazo@gmail.com>
2023-06-03 18:06:21 -0300

    clases negocio y datos

---
+++ TP8_Grupo_06/Datos/Conexion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Datos
+{
+    public class Conexion
+    {
+        public static string ruta = @"Data Source=localhost\sqlexpress; Initial Catalog = BDSucursales; Integrated Security = True";
+        public DataTable ObtenerTablas(string consultaSQL, string NombreTabla)
+        {
+            SqlConnection conex = new SqlConnection(ruta);
+            conex.ConnectionString = ruta;
+            conex.Open();
+            SqlDataAdapter adap = new SqlDataAdapter(consultaSQL, conex);
+            DataSet data = new DataSet();
+            adap.Fill(data, NombreTabla);
+            conex.Close();
+            return data.Tables[NombreTabla];
+        }
+
+        public int RealizarConsulta(string consultaSQL)
+        {
+            SqlConnection cn = new SqlConnection(ruta);
+            cn.Open();
+            SqlCommand cmd = new SqlCommand(consultaSQL, cn);
+            int Rows = (int)cmd.ExecuteNonQuery();
+            cn.Close();
+            return Rows;
+        }
+    }
+}
---
+++ TP8_Grupo_06/Negocio/gestorSucursales.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Datos;
+
+namespace Negocio
+{
+    public class gestorSucursales
+    {
+        Conexion ruta = new Conexion();
+
+        public int AgregarSucursal(string nombre, string descripcion, string idProvincia, string direccion)
+        {
+
+            string consultaSQL = "INSERT INTO Sucursal (NombreSucursal, DescripcionSucursal, Id_ProvinciaSucursal, DireccionSucursal) VALUES ('" + nombre + "', '" + descripcion + "', " + idProvincia + ", '" + direccion + "')";
+            return ruta.RealizarConsulta(consultaSQL);
+
+        }
+    }
+}
