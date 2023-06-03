1a56e880d770748f8301e007f37fcab111790ea2
MNRocha <mnrrocha@hotmail.com>
2023-06-03 12:07:15 -0300

    TP8: Clase Sucursal y Referencias

---
+++ TP8_Grupo_06/Entidades/Sucursal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Sucursal
+    {
+        private int id;
+        private string nombre;
+        private string descripcion;
+        private int idHorario;
+        private int idProvincia;
+        private string direccion;
+        private string URLimagen;
+        public Sucursal()
+        {
+
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+        public int IdHorario { get => idHorario; set => idHorario = value; }
+        public int IdProvincia { get => idProvincia; set => idProvincia = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public string URLimagen1 { get => URLimagen; set => URLimagen = value; }
+    }
+}
